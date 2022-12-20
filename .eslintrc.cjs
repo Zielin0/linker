9c6131bcafa3764ebfb04548cfb856b440405270
Zielin0 <me@zielinus.xyz>
2022-12-21 00:22:35 +0100

    feat(*): Hello linker 🚀

---
+++ .eslintrc.cjs
@@ -0,0 +1,11 @@
+module.exports = {
+  root: true,
+  extends: [
+    '@nuxtjs/eslint-config-typescript',
+    '@vue/eslint-config-airbnb-with-typescript',
+    'plugin:prettier/recommended',
+  ],
+  rules: {
+    'import/no-extraneous-dependencies': 0,
+  },
+};
---
+++ nuxt.config.ts
@@ -0,0 +1,8 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+
+export default defineNuxtConfig({
+  css: ['vuetify/styles/main.sass', '@mdi/font/css/materialdesignicons.css'],
+  build: {
+    transpile: ['vuetify'],
+  },
+});
