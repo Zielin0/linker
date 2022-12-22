37663b652bdbe2d07df8d08bce4f481f4a6c2274
Zielin0 <me@zielinus.xyz>
2022-12-22 14:39:22 +0100

    chore(index): add zielinus.xyz link

--- pages/index.vue
+++ pages/index.vue
@@ -7,6 +7,19 @@
     <v-layout class="mt-10 d-flex justify-center text-center">
       <v-hover v-slot="{ isHovering, props }">
         <NuxtLink
+          class="text-decoration-none mr-2"
+          :class="
+            isHovering ? 'text-primary text-decoration-underline' : 'text-white'
+          "
+          v-bind="props"
+          to="https://www.zielinus.xyz"
+        >
+          zielinus.xyz
+        </NuxtLink>
+      </v-hover>
+
+      <v-hover v-slot="{ isHovering, props }">
+        <NuxtLink
           class="text-decoration-none"
           :class="isHovering ? 'text-primary' : 'text-white'"
           v-bind="props"
