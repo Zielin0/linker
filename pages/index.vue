57adb4b793f2d020c7324ec55fb49a2d9a89062d
Zielin0 <me@zielinus.xyz>
2022-12-24 22:31:10 +0100

    feat(index): display all public links in table

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,70 @@
+<script lang="ts">
+import { Link } from '@prisma/client';
+
+type Response = {
+  success: boolean;
+  message?: string;
+  links?: Link[];
+};
+
+const getPublicLinks = async (): Promise<Response> => {
+  const { data } = await useFetch('/api/public');
+  return data.value as Response;
+};
+
+const linkArr: Link[] = [];
+
+export default {
+  data: () => ({
+    links: linkArr,
+    success: false,
+  }),
+  created() {
+    getPublicLinks().then((res) => {
+      this.success = JSON.stringify(res.success) === 'true';
+      this.links = res.links!;
+    });
+  },
+};
+</script>
+
 <template>
   <v-container class="h-100" fluid>
     <v-layout class="mt-10 d-flex justify-center text-center">
       <h1>All Public Links</h1>
     </v-layout>
-    <!-- Table goes in here -->
+
+    <v-layout class="d-flex justify-center text-center mt-8">
+      <v-table v-if="$data.success" class="w-50">
+        <thead>
+          <tr>
+            <th class="text-left">ID</th>
+            <th class="text-left">Alias</th>
+            <th class="text-left">Target URL</th>
+            <th class="text-left">Views</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="link in links" :key="link.alias">
+            <td class="text-left">{{ link.id }}.</td>
+            <td class="text-left">
+              <NuxtLink class="text-white" :to="`/${link.alias}`">{{
+                link.alias
+              }}</NuxtLink>
+            </td>
+            <td class="text-left">
+              <NuxtLink class="text-white" :to="link.original">{{
+                link.original
+              }}</NuxtLink>
+            </td>
+            <td class="text-left">{{ link.views }}</td>
+          </tr>
+        </tbody>
+      </v-table>
+
+      <span v-else class="text-red">Failed to fetch all public links.</span>
+    </v-layout>
+
     <v-layout class="mt-10 d-flex justify-center text-center">
       <v-hover v-slot="{ isHovering, props }">
         <NuxtLink
